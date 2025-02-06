74c711f5a1c3fe6eaca68b467bcb35dbda3c108a
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-06 14:30:43 -0300

    update Sexo

--- src/main/java/cadastroPessoas/backend/domain/entities/enums/Sexo.java
+++ src/main/java/cadastroPessoas/backend/domain/entities/enums/Sexo.java
@@ -4,4 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum Sexo {
+    MASCULINO("MASCULINO"),
+    FEMININO("FEMININO"),
+    NAOINFORM("PREFIRO NAO DIZER"),
+    OUTRO("OUTRO");
+    private final String nome;
+    Sexo(String nome){
+        this.nome=nome;
+    }
 }
