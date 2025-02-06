bce3778697c4b548b93459fa6b166ca92995aa04
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-06 14:31:00 -0300

    update TelefoneTipo.java

--- src/main/java/cadastroPessoas/backend/domain/entities/enums/TelefoneTipo.java
+++ src/main/java/cadastroPessoas/backend/domain/entities/enums/TelefoneTipo.java
@@ -1,4 +1,17 @@
 package cadastroPessoas.backend.domain.entities.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum TelefoneTipo {
+    PESSOAL("PESSOAL"),
+    RESIDENCIAL("RESIDENCIAL"),
+    OUTROS("OUTROS");
+
+    private String name;
+
+    TelefoneTipo(String name){
+        this.name=name;
+    }
+
 }
