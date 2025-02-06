c5869365851f0db5f00e538fc92cf1ab50f2e08f
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-02-06 14:34:07 -0300

    update Endereco.java

--- src/main/java/cadastroPessoas/backend/domain/entities/Endereco.java
+++ src/main/java/cadastroPessoas/backend/domain/entities/Endereco.java
@@ -1,4 +1,41 @@
 package cadastroPessoas.backend.domain.entities;
 
+import cadastroPessoas.backend.domain.entities.enums.EstadoBr;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Table(name = "tb_endereco")
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@AllArgsConstructor
+@Entity
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Endereco {
+    @Column(nullable = false, unique = false, length = 100)
+    private String logradouro;
+    @Column(nullable = false, unique = false, length = 10)
+    private String numero;
+    @Column(nullable = false, unique = false, length = 100 )
+    private String complemento;
+    @Column(nullable = false, unique = false, length = 100 )
+    private String bairro;
+    @Column(nullable = false, unique = false, length = 100)
+    private String cidade;
+    @Column(nullable = false, unique = false, length = 20)
+    private EstadoBr estado;
+    @NotNull
+    @NotBlank
+    @Column(nullable = false, unique = false, length = 10)
+    @Pattern(regexp = "\\d{5}-\\d{3}", message = "O campo 'cep' deve ter o formato '12345-678'")
+    private String cep;
 }
